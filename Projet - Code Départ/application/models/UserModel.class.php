6fb8d88007d01926802bc86bddcd9b850ea35a95
ben <benjamin.masi45@gmail.com>
2017-12-11 16:34:30 +0100

    Ajout de la class Login - vérification des password et e-mail saisis dans le formulaire de connexion via la DB fonctionnel. A faire : gestion des cas de connexion en fonction des droits d'accès et génération des liens

--- Projet - Code Départ/application/models/UserModel.class.php
+++ Projet - Code Départ/application/models/UserModel.class.php
@@ -2,14 +2,12 @@
 
 class UserModel extends AbstractModel
 {
-  const SQL = "SELECT `mail` FROM `user` as U INNER JOIN `password` as P on P.user_id=U.id WHERE mail=? LIMIT 1";
+  const SQL = "SELECT `mail`, `password`, `rights` FROM `user` as U INNER JOIN `password` as P on P.user_id=U.id WHERE mail=? LIMIT 1";
 
   public function login($queryFields)
   {
-    var_dump($queryFields);
     $values = [$queryFields[0]];
     $result = $this->database->queryOne(self::SQL, $values);
-    var_dump($result);
     return($result);
   }
 
