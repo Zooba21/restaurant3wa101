12fc0d18662b6401aefd05b8ca6105b861b5157c
Ben <benjamin.masi45@gmail.com>
2017-12-12 10:06:09 +0100

    Mise à jour architecture des fichiers

--- application/controllers/login/LoginController.class.php
+++ application/controllers/login/LoginController.class.php
@@ -10,16 +10,18 @@ class LoginController
   public function httpPostMethod(Http $http, array $queryFields)
   {
     $login = new UserModel(new Database);
+    $target = new Http;
 
     $loginForm = [$queryFields['email']];
     $result = $login->login($loginForm);
 
     if ($queryFields['email'] == $result['mail'] && $queryFields['passwd'] == $result['password'])
     {
-      
+
       if ($result['rights'] == "Employed")
       {
-
+        $_SERVER['id']=$result['id'];
+        $_SERVER['rights']=$result['rights'];
       }
       else
       {
