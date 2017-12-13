a796e38a3def89e34a077d64ae64f3df001db7d1
Ben <benjamin.masi45@gmail.com>
2017-12-13 14:43:49 +0100

    ajout des champs <name> aux inputs de RegistreForm

--- application/controllers/registre/RegistreController.class.php
+++ application/controllers/registre/RegistreController.class.php
@@ -4,12 +4,13 @@ class RegistreController
 {
   public function httpGetMethod(Http $http, array $queryFields)
   {
-    var_dump($queryFields);
-    $account = new AccountCreationModel;
+
   }
   public function httpPostMethod(Http $http, array $queryFields)
   {
-
+    var_dump($_POST);
+    var_dump($queryFields);
+    $account = new AccountCreationModel;
   }
 }
 
