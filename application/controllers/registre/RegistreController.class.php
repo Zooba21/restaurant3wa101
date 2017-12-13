0bff92b6f4d07d834d7384dc91a18c5979e7815e
Ben <benjamin.masi45@gmail.com>
2017-12-13 14:34:07 +0100

    Mise à jour Registre viewer

--- application/controllers/registre/RegistreController.class.php
+++ application/controllers/registre/RegistreController.class.php
@@ -4,7 +4,8 @@ class RegistreController
 {
   public function httpGetMethod(Http $http, array $queryFields)
   {
-
+    var_dump($queryFields);
+    $account = new AccountCreationModel;
   }
   public function httpPostMethod(Http $http, array $queryFields)
   {
--- application/models/AccountCreationModel.class.php
+++ application/models/AccountCreationModel.class.php
@@ -2,31 +2,31 @@
 
 Class AccountCreationModel
 {
-  const SQLUser = "INSERT INTO `user`(`name`, `firstName`, `address`, `address2`, `postCode`, `city`, `phone`, `mail`) VALUES=(?,?,?,?,?,?,?,?)";
+  const SQLUser = "INSERT INTO `user`(`name`, `firstName`, `mail`) VALUES=(?,?,?)";
 
-  const SQLPassword = "INSERT INTO `password`(`password`,`userId`) VALUES=?,?";
+  const SQLPassword = "INSERT INTO `password`(`password`,`userId`) VALUES=()?,?)";
 
-  const SQLUserRessources "INSERT INTO `userRessources`(`url`,`alt`,`userId`) VALUES=?,?,?";
+  const SQLUserRessources = "INSERT INTO `userRessources`(`url`,`alt`,`userId`) VALUES=(?,?,?)";
 
-  function public createUser(array $queryFields)
+  public function createUser(array $queryFields)
   {
     var_dump($queryFields);
-//    $result = $this->database->executeSql(self::SQLUser, $queryFields);
-//    var_dump($result);
+    $result = $this->database->executeSql(self::SQLUser, $queryFields);
+    var_dump($result);
     return($result);
   }
 
-  function public createPassword(array $queryFields)
+  public function createPassword(array $queryFields)
   {
-    var_dump($queryFields),
+    var_dump($queryFields);
     //    $result = $this->database->executeSql(self::SQLPassword, $queryFields);
     //    var_dump($result);
     return($result);
   }
 
-  function public createAvatar(array $queryFields, array $fileInput)
+  public function createAvatar(array $queryFields, array $fileInput)
   {
-    var_dump($queryFields),
+    var_dump($queryFields);
     //    $result = $this->database->executeSql(self::SQLUserRessources, $queryFields);
     //    var_dump($result);
     return($result);
