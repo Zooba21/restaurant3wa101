13751a2ef8374ec926c8ce6429b053e2dc7be14f
SebastienLeger02 <leger02@gmail.com>
2017-12-14 16:10:08 +0100

    Petit création de "itemSold" pour le Menu

--- application/controllers/menu/MenuController.class.php
+++ application/controllers/menu/MenuController.class.php
@@ -4,6 +4,18 @@ class MenuController
 {
   public function httpGetMethod(Http $http, array $queryFields)
   {
+    $render = (new UserSession)->getAll();// Permet de rester connecté
+    $render['flashbag']= new FlashBag;// Mettre sur toutes les pages
+
+$menu = new ItemSoldModel(new Database) ;
+$enumList = ['entrée','plat','dessert','boisson'];
+foreach($enumList as $key=>$value)
+{
+  $render['menu']["$value"] = $menu->getMeal(["$value"]);
+}
+
+var_dump($render);
+    return $render;
 
   }
   public function httpPostMethod(Http $http, array $queryFields)
